e43ba0bceb91083a52d84f02fbe9aac8bc8769ef
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-03 14:17:29 -0500

    add dispatch and update query

--- src/redux/api.js
+++ src/redux/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const { REACT_APP_API_ID, REACT_APP_API_KEY } = process.env;
 
 export const getRecipes = async(query) => {
-  console.log(query)
   const url = `https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_API_ID}&app_key=${REACT_APP_API_KEY}`;
   return await axios.get(url);
 };
