06208b77e9d214677898acf1151fb51d05554030
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-03 23:14:41 -0500

    render card recipe

--- src/components/recipeItem/Recipe.js
+++ src/components/recipeItem/Recipe.js
@@ -1,9 +1,99 @@
-const Recipe = ({ label }) => {
-  console.log(label)
+import { useState } from "react";
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, Grid, IconButton, Typography } from "@material-ui/core";
+import { MoreVert, Favorite, Share, ExpandMore, DirectionsRun } from '@material-ui/icons';
+import { red } from '@material-ui/core/colors';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  }
+}));
+
+const Recipe = ({ calories, image, label, ingredients }) => {
+  const [expanded, setExpanded] = useState(false);
+  const classes = useStyles();
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div>
-      <h3>{label}</h3>
-    </div>
+    <Grid item>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              {label[0].toUpperCase()}
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title={label}
+          subheader={
+            <span>
+              <DirectionsRun />
+              {calories}
+            </span>
+          }
+        />
+        <CardMedia
+          image={image}
+          alt={label}
+          className={classes.media}
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Favorite />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMore />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph variant="h4">Ingredients:</Typography>
+            {
+              ingredients.map((item) => (
+                <Typography key={item.food} paragraph>{item.text}</Typography>
+              ))
+            }
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
   )
 };
 
